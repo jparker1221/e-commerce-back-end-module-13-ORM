f0a3927cd4a4847ebb5cd88beb8d27c8ee2e32a7
jparker1221 <jonparker1221@gmail.com>
2023-06-22 17:01:31 -0400

    partially completed category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,15 +6,40 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [{ model: Product }]
+  }).then((categoryData) => {
+    res.status(200).json(categoryData)
+  }).catch((err) => {
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, {
+    include: [{model: Product}]
+  }).then((categoryData) => {
+    if (!categoryData) {
+      return res.status(404).json({
+        message: "Sorry, couldn't find the category by that id!"
+      })
+    }
+    res.status(200).json(categoryData)
+  }).catch((err) => {
+    res.status(500).json(err)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((categoryData) => {
+    res.status(200).json(categoryData)
+  }).catch((err) => {
+    res.status(500).json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
